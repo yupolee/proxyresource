57508a8e144a85a550fac3b9be2aee5928919c33
Chris Lee <sunshy.cheer@gmail.com>
2025-08-27 18:29:43 +0800

    Fix EmbyChameleon.js

--- Script/EmbyChameleon.js
+++ Script/EmbyChameleon.js
@@ -2,8 +2,9 @@
 #!name= EmbyChameleon
 #!desc= 解除Emby观看客户端限制
 #!author= Chris
+#!usage= 以Loon为例，添加到Script部分并配置args
 [Script]
-http-request ^https?:\/\/emby\.xxx script-path=EmbyChameleon.js, timeout = 5, tag=EmbyChameleon, args="client=&device=&deviceId=&version="
+http-request ^https?:\/\/emby\.xxx script-path=EmbyChameleon.js, timeout = 5, tag=EmbyChameleon, args="client=&version=&token=&device=&deviceId="
 
 [MITM]
 hostname = emby.xxx
@@ -11,34 +12,62 @@ hostname = emby.xxx
 const isRequest = typeof $request !== "undefined";
 const hasArgs = typeof $argument !== "undefined";
 
+let headers = $request.headers;
+let url = $request.url;
 
-if (!hasArgs) {$done({});};
+function setHeader(obj, key, value) {
+    const realKey = Object.keys(obj).find(
+        k => k.toLowerCase() === key.toLowerCase()
+    );
+    if (realKey) obj[realKey] = value;
+};
+
+function getHeader(obj, key) {
+    const realKey = Object.keys(obj).find(
+        k => k.toLowerCase() === key.toLowerCase()
+    );
+    if (realKey) return obj[realKey];
+};
+
+function isSafari(headers) {
+    const ua = getHeader(headers, "user-agent").toLowerCase();
+    return ua.includes("iphone") && ua.includes("safari") && ua.includes("mobile");
+};
+
+if (isSafari(headers)) $done({});
+if (!hasArgs) $done({});
+
+const parseArgument = argu =>
+    (argu || '').split('&').filter(Boolean).reduce((o, kv) => {
+        const [k, v = ''] = kv.split('=');
+        o[k] = v;
+        return o;
+    }, {});
+const argu = parseArgument($argument);
 
 if (isRequest) {
-
-    let headers = JSON.parse($request.headers);
-    let url = $request.url;
-
     if (url.includes("emby/videos")) {
-        headers["User-Agent"] = "libmpv";
+        setHeader(headers, "user-agent", "libmpv");
+
+        ["X-Emby-Authorization", "X-Emby-Token"].forEach(k => delete headers[k]);
         $done({headers: headers});
     };
 
-    if (headers.indexOf("x-emby-authorization") != -1) {
+    if (Object.hasOwn(headers, "x-emby-authorization")) {
         var auth = headers["x-emby-authorization"].replaceAll("\"", "");
 
-        if (headers.indexOf("x-emby-token") != -1) {
-            auth = auth.replace(/^(?!MediaBrowser)/, "MediaBrowser Token=".concat(headers["x-emby-token"], ", "));
+        if (Object.hasOwn(headers, "x-emby-token")) {
+            headers["x-emby-token"] = argu.token;
+            auth = auth.replace(/^(?!MediaBrowser)/, "MediaBrowser Token=".concat(argu.token, ", "));
         };
-        auth = auth.replace(/Client=[\w-]+/i, "Client=".concat($argument.client));
-        auth = auth.replace(/Device=[\w-]+/i, "Device=".concat($argument.device));
-        auth = auth.replace(/DeviceId=[\w-]+/i, "DeviceId=".concat($argument.deviceId));
-        auth = auth.replace(/Version=[\d\.]+/i, "Version=".concat($argument.version));
+        auth = auth.replace(/Client=[\w-]+/i, "Client=".concat(argu.client));
+        auth = auth.replace(/Device=[\w-]+/i, "Device=".concat(argu.device));
+        auth = auth.replace(/DeviceId=[\w-]+/i, "DeviceId=".concat(argu.deviceId));
+        auth = auth.replace(/Version=[\d\.]+/i, "Version=".concat(argu.version));
 
         headers["x-emby-authorization"] = auth;
-        headers["user-agent"] = $argument.client.concat("/", $argument.version);
-
-        $done({headers: JSON.stringify(headers)});
     };
 
+    setHeader(headers, "user-agent", argu.client.concat("/", argu.version));
+    $done({headers: headers});
 };
